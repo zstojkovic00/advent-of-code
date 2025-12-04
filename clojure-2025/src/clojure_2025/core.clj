85964da44bc96c2e4f4e256950c040c9479a63ea
zstojkovic00 <00zeljkostojkovic@gmail.com>
2025-12-04 02:19:27 +0100

    clojure2025 aoc part1 done

--- clojure-2025/src/clojure_2025/core.clj
+++ clojure-2025/src/clojure_2025/core.clj
@@ -6,18 +6,75 @@
 
 ;; treba mi funkcija koja prihvata vektor i za svaki clan tog vektora racuna da li nakon pomeranja u stranu je jednako 0
 ;; 1. Otvori fajl
-;; 2. Parsiraj fajl
-;; 3. za svaki element liste odredi stranu i to dodaj ili oduzmi do 50, left je minus a right je + sa time sto
-;; 50 - 68 = -18 abs()
-;; 50 + 55 = 100 + 5 - 100
+;; 2. Parsiraj fajl (L68 -> Left 68, R22 -> Right 22)
+;; 3. Za svaki element uzmi direction i oduzmi/dodaj na trenutnu poziciju nakon toga posto je ciklican proces uradi mod 100
 ;; 4. Uporedi da li je rezultat 0, ako jeste inc
-
-
 
 (System/getProperty "user.dir")
 (slurp "src/clojure_2025/tasks/test1.txt")
+(slurp "https://adventofcode.com/2025/day/1/input") ;; ne radi :/ vrv moram da budem ulogovan
 
+;; ucitavanje fajla
 (def lines
-  (str/split-lines (slurp "src/clojure_2025/tasks/test1.txt")))
+  (str/split-lines (slurp "src/clojure_2025/tasks/input1.txt")))
 
 (identity lines)
+
+;; parsiranje fajla
+(defn parse? [s] (
+                   let [direction (subs s 0 1)
+                        rest (Integer/parseInt (subs s 1))]
+                   [direction rest]
+                   ))
+
+(subs "L68" 0 1)
+(subs "L68" 1)
+(parse? "L68")
+
+(map parse? lines)
+(mod -18 100)
+
+
+;; Logika
+(defn rotate [[direction num] cnt]
+  (mod (if (= direction "L")
+         (- cnt num)
+         (+ cnt num))
+       100)
+  )
+
+;; tests
+(rotate ["L" 68] 50)
+(rotate ["L" 20] 50)
+(rotate  ["L" 50] 50)
+
+(rotate ["R" 68] 50)
+(rotate ["R" 68] 20)
+(rotate  ["R" 50] 50)
+(rotate ["R" 155] 50)
+
+
+;; reduce vraca samo poslednji rezultat
+(reduce (fn [cnt pair]
+          (rotate pair cnt))
+        50
+        (map parse? lines))
+
+
+(count (filter zero? (reductions (fn [cnt pair]
+                                   (rotate pair cnt))
+                                 50
+                                 (map parse? lines))))
+
+
+
+
+
+
+
+
+
+
+
+
+
