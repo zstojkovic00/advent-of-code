603cea5943dd053c6f55919ecea86edc14e0b446
zstojkovic00 <00zeljkostojkovic@gmail.com>
2025-12-15 00:08:58 +0100

    aoc in kotlin day5 part1 done

--- kotlin-2025/src/main/kotlin/Day5.kt
+++ kotlin-2025/src/main/kotlin/Day5.kt
@@ -4,5 +4,26 @@ import java.io.File
 
 fun main(){
 
-    val lines = File("src/main/resources/test5.txt").readLines()
+    val ingredients = File("src/main/resources/input5.txt").readLines()
+    val freshIngredientsRanges = mutableListOf<LongRange>()
+    val availableIngredients = mutableListOf<Long>()
+
+    for(ingredient in ingredients){
+
+        if(ingredient.isEmpty()){
+            continue
+        }
+
+        if(ingredient.contains("-")){
+            val parts = ingredient.split("-", limit = 2)
+            freshIngredientsRanges.add(parts[0].toLong()..parts[1].toLong())
+        } else {
+            availableIngredients.add(ingredient.toLong())
+        }
+    }
+
+    val count = availableIngredients.count { ingredient ->
+        freshIngredientsRanges.any { range -> ingredient in range }
+    }
+    println(count)
 }
