7b5619cba1fe0e1b0de3bddc90dbf3e65ef5f465
zstojkovic00 <00zeljkostojkovic@gmail.com>
2025-12-21 16:52:12 +0100

    aoc in kotlin day6 part2 not working :/

--- kotlin-2025/src/main/kotlin/Day6.kt
+++ kotlin-2025/src/main/kotlin/Day6.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 
 fun main() {
-    val lines = File("src/main/resources/input6.txt").readLines()
+    val lines = File("src/main/resources/test6.txt").readLines()
     val problems = mutableMapOf<Int, Problem>()
 
     for ((i, line) in lines.withIndex()) {
@@ -40,7 +40,56 @@ fun main() {
     }
 
     println(count)
+
+    val problemsWithPadding = mutableListOf<Problem>()
+
+    for (problem in problems.values.reversed()) {
+        val stringNumbers = problem.numbers.map { it.toString() }
+        val maxLengthPerNumbers: Int = stringNumbers.maxOf { it.length }
+
+        val padded = stringNumbers.map { number ->
+            if (number.length < maxLengthPerNumbers) {
+                val padding = "0".repeat(maxLengthPerNumbers - number.length)
+                number + padding
+            } else {
+                number
+            }
+        }
+
+        val paddedAsLong = padded.map { it.toLong() }.toMutableList()
+        problemsWithPadding.add(Problem(paddedAsLong, problem.operator))
+    }
+
+    var result = 0L
+
+    for (problem in problemsWithPadding) {
+        val rightToLeftNumbers = mutableMapOf<Int, MutableList<Char>>()
+        println(problem.numbers)
+        for (number in problem.numbers.map { it.toString() }) {
+            for ((i, c) in number.reversed().withIndex()) {
+                if (c != '0') {
+                    rightToLeftNumbers.getOrPut(i) { mutableListOf() }.add(c)
+                }
+            }
+        }
+
+        println(rightToLeftNumbers)
+
+        val rightToLeftNumbersLong = rightToLeftNumbers.values.map { charList ->
+            charList.joinToString("").toLong()
+        }
+
+        println(rightToLeftNumbersLong)
+
+        if (problem.operator == "*") {
+            result = rightToLeftNumbersLong.reduce { acc, num -> acc * num }
+        } else if (problem.operator == "+") {
+            result = rightToLeftNumbersLong.sum()
+        }
+
+    }
+
+    println(result)
 }
 
 data class Problem(val numbers: MutableList<Long>, val operator: String?)
-
