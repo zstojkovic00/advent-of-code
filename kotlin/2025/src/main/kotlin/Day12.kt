f9cf98e572d6cc07c3ec7b8554c3da728a448238
zstojkovic00 <00zeljkostojkovic@gmail.com>
2025-12-28 22:19:28 +0100

    aoc in kotlin day12 part1 done

--- kotlin/2025/src/main/kotlin/Day12.kt
+++ kotlin/2025/src/main/kotlin/Day12.kt
@@ -4,6 +4,28 @@ import readInputLines
 
 
 fun main() {
-    val lines = readInputLines(false, 2025, 12)
-    println(lines)
+    val lines = readInputLines(false, 2025, 12).drop(30)
+    var possible = 0
+
+    for (line in lines) {
+        if (line.isBlank()) continue
+
+        val parts = line.split(": ")
+        val dimensions = parts[0].split("x")
+        val length = dimensions[0].toInt()
+        val width = dimensions[1].toInt()
+
+        val nums = parts[1].trim().split(" ").map { it.toInt() }
+
+        var naiveUpper = 0
+        for (i in 1 until nums.size) {
+            naiveUpper += nums[i]
+        }
+
+        if (naiveUpper <= length / 3 * width / 3) {
+            possible++
+        }
+    }
+
+    println(possible)
 }
